98cdbad3b55a19a4b1f5edb12079a3e97a868d47
giaccneto <giaccento@hotmail.com>
2025-04-18 09:10:43 -0400

    Criado esqueleto do projeto

---
+++ src/main/java/com/giaccneto/bff_agendador_tarefa/BffAgendadorTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.giaccneto.bff_agendador_tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefaApplication.class, args);
+	}
+
+}
